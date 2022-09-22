8740ac6a73105520c223f18e1b719e1256684868
Edix505 <101802740+Edix505@users.noreply.github.com>
2022-09-22 22:48:08 +0200

    13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - function that checks if a node
- * @node: pointer to the node to check.
+ * binary_tree_leaves - Counts the leaves in a binary tree
+ * @tree: Pointer to the root node of the tree
  *
- * Return: 1 if node 
+ * Return: If the tree is NULL, must return 0
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
